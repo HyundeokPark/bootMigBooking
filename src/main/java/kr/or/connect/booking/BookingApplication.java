4d2294f8dc76da1099a841a940476269e2a2759c
ares <rphd0923@gmail.com>
2022-09-28 19:41:37 +0900

    complete

---
+++ src/main/java/kr/or/connect/booking/BookingApplication.java
@@ -0,0 +1,11 @@
+package kr.or.connect.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookingApplication.class, args);
+    }
+}
---
+++ src/test/java/kr/or/connect/booking/BookingApplicationTest.java
@@ -0,0 +1,14 @@
+package kr.or.connect.booking;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingApplicationTest {
+
+    @Test
+    @DisplayName("contextLoad test")
+    void main() {
+    }
+}
